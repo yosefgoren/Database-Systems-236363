9404310e798224bb82dbd58441527af9f9f26d6d
unknown <yonatan>
2023-06-08 01:46:44 +0300

    added most of Basic API and tests

--- Homework2/code/Solution.py
+++ Homework2/code/Solution.py
@@ -9,14 +9,68 @@ from psycopg2 import sql
 
 #GLOBAL SETTINGS:
 
-print_logs = True
-
+print_logs = False
+print_exceptions = False
 
 #GLBOAL VARIABLES:
-ptable_name = "ptable"
-dtable_name = "dtable"
-rtable_name = "rtable"
+ptable = "ptable"
+dtable = "dtable"
+rtable = "rtable"
+podtable = "podtable"
+rodtable = "rodtable"
 
+table_specifications = {
+    ptable: [
+        "photoID INT NOT NULL",
+        "description TEXT NOT NULL",
+        "size INT NOT NULL",
+        "PRIMARY KEY(photoID)",
+        "CHECK (photoID > 0)",
+        "CHECK (size >= 0)"
+    ],
+    dtable: [
+        "diskID INT NOT NULL",
+        "company TEXT NOT NULL",
+        "speed INT NOT NULL",
+        "free_space INT NOT NULL",
+        "cost INT NOT NULL",
+        "PRIMARY KEY(diskID)",
+        "CHECK (diskID > 0)",
+        "CHECK (speed > 0)",
+        "CHECK (cost > 0)",
+        "CHECK (free_space >= 0)"
+    ],
+    rtable: [
+        "ramID INT NOT NULL",
+        "company TEXT NOT NULL",
+        "size INT NOT NULL",
+        "PRIMARY KEY (ramID)",
+        "CHECK (ramID > 0)",
+        "CHECK (size > 0)"
+    ],
+    podtable: [
+        "photoID INT NOT NULL",
+        "diskID INT NOT NULL",
+        f"FOREIGN KEY (photoID) REFERENCES {ptable}(photoID) ON DELETE CASCADE",
+        f"FOREIGN KEY (diskID) REFERENCES {dtable}(diskID) ON DELETE CASCADE",
+        "PRIMARY KEY(photoID, diskID)"
+    ],
+    rodtable : [
+        "ramID INT NOT NULL",
+        "diskID INT NOT NULL",
+        f"FOREIGN KEY (ramID) REFERENCES {rtable}(ramID) ON DELETE CASCADE",
+        f"FOREIGN KEY (diskID) REFERENCES {dtable}(diskID) ON DELETE CASCADE",
+        "PRIMARY KEY(ramID, diskID)"
+    ]
+}
+
+def log_exception(*s):
+    if print_exceptions:
+        print(*s)
+
+def create_values_exp(*fields: list)->str:
+    new_fields = [str(fld) if type(fld) != str else "'"+fld+"'" for fld in fields]
+    return f"VALUES({strsum(new_fields)})"
 
 #AUX FUNCTIONS:
 def strsum(strings: list, delim = ", ", delim_last = False)->str:
@@ -37,75 +91,65 @@ def sql_exe(query: str):
         returns 'False' when running successful,
         and 'True' if a DB exception was raised.
     """
+    if len(query)>0 and query[-1] != ";":
+        query += ";"
     log_query(query)
     result = None
     try:
         conn = Connector.DBConnector()
         result = conn.execute(query)
         conn.commit()
+    except DatabaseException.FOREIGN_KEY_VIOLATION:
+        log_exception("seen FOREIGN_KEY_VIOLATION")
+        return ReturnValue.NOT_EXISTS, result #TODO: check what should be here.
     except DatabaseException.UNIQUE_VIOLATION:
+        log_exception("seen UNIQUE_VIOLATION")
         return ReturnValue.ALREADY_EXISTS, result
     except DatabaseException.ConnectionInvalid:
+        log_exception("seen ConnectionInvalid")
         return ReturnValue.ERROR, result
-    except DatabaseException.FOREIGN_KEY_VIOLATION:#when this happends?
-        return ReturnValue.ERROR, result #TODO: check what should be here.
     except DatabaseException.database_ini_ERROR:
+        log_exception("seen database_ini_ERROR")
         return ReturnValue.ERROR, result
     except DatabaseException.UNKNOWN_ERROR:
+        log_exception("seen UNKNOWN_ERROR")
         return ReturnValue.ERROR, result
     except DatabaseException.NOT_NULL_VIOLATION:
+        log_exception("seen NOT_NULL_VIOLATION")
         return ReturnValue.BAD_PARAMS, result
     except DatabaseException.CHECK_VIOLATION:
+        log_exception("seen CHECK_VIOLATION")
         return ReturnValue.BAD_PARAMS, result
     return ReturnValue.OK, result
 
+def sql_exe_transcation(*queries: list):
+    """
+        assumes the queries provided do not include ';'.
+    """
+    assert(all([(len(q) > 0) and (q[-1] != ";") for q in queries]))
+    query = strsum(["BEGIN"]+[q for q in queries]+["END"], ";\n", True)
+    return sql_exe(query)
+
 #CURD API:
 def createTables():
+    #TODO: check that if we need to handle table-already exists error or should ignore.
     def create_table_format(tname: str, scheme_attributes: list)->str:
         if len(scheme_attributes) < 1:
             raise "got empty list of attributes"
         scheme = strsum(scheme_attributes)
-        return f"CREATE TABLE {tname}({scheme});"
-    table_specifications = {
-        ptable_name: [
-            "photoID INT NOT NULL",
-            "description TEXT NOT NULL",
-            "size INT NOT NULL",
-            "PRIMARY KEY(photoID)",
-            "CHECK (photoID > 0)",
-            "CHECK (size >= 0)"
-        ],
-        dtable_name: [
-            "diskID INT NOT NULL",
-            "company TEXT NOT NULL",
-            "speed INT NOT NULL",
-            "free_space INT NOT NULL",
-            "cost INT NOT NULL",
-            "PRIMARY KEY(diskID)",
-            "CHECK (diskID > 0)",
-            "CHECK (speed > 0)",
-            "CHECK (cost > 0)",
-            "CHECK (free_space >= 0)"
-        ],
-        rtable_name: [
-            "ramID INT NOT NULL",
-            "company TEXT NOT NULL",
-            "size INT NOT NULL",
-            "PRIMARY KEY (ramID)",
-            "CHECK (ramID > 0)",
-            "CHECK (size > 0)"
-        ]
-    }
+        return f"CREATE TABLE {tname}({scheme})"
+    
     create_table_queries = [create_table_format(tname, scheme) for tname, scheme in table_specifications.items()]
-    create_all_query = "BEGIN;\n"+strsum(create_table_queries, "\n", True)+"COMMIT;"
-    sql_exe(create_all_query)
+    sql_exe_transcation(*create_table_queries)
 
 def clearTables():
-    pass
+    sql_exe_transcation(*[f"DELETE FROM {tname}" for tname in table_specifications.keys().__reversed__()])
 
 def dropTables():
-    pass
+    sql_exe_transcation(*[f"DROP TABLE {tname}" for tname in table_specifications.keys().__reversed__()])
 
+def addPhoto_query(photo: Photo)->str:
+    return f"INSERT INTO {ptable} {create_values_exp(photo.getPhotoID(), photo.getDescription(), photo.getSize())}"
 
 def addPhoto(photo: Photo) -> ReturnValue:
     """
@@ -113,20 +157,22 @@ def addPhoto(photo: Photo) -> ReturnValue:
         description TEXT NOT NULL
         size INT NOT NULL
     """
-    ret, res = sql_exe(f"INSERT INTO {ptable_name} VALUES({photo.getPhotoID()}, '{photo.getDescription()}', {photo.getSize()});")
+    ret, res = sql_exe(addPhoto_query(photo))
     return ret
 
 def getPhotoByID(photoID: int) -> Photo:
-    ret, (num_results, results) = sql_exe(f"SELECT * FROM {ptable_name} WHERE photoID = {photoID};")
+    ret, (num_results, results) = sql_exe(f"SELECT * FROM {ptable} WHERE photoID = {photoID};")
     if len(results) < 1:
         return Photo.badPhoto()
     return Photo(*(results[0].values()))
 
 def deletePhoto(photo: Photo) -> ReturnValue:
     #TODO: add cascade properties to DB to cause automatic deletion of other items...
-    ret, res = sql_exe(f"DELETE FROM {ptable_name} WHERE photoID = {photo.getPhotoID()};")
+    ret, res = sql_exe(f"DELETE FROM {ptable} WHERE photoID = {photo.getPhotoID()};")
     return ret
 
+def addDisk_query(disk: Disk)->str:
+    return f"INSERT INTO {dtable} {create_values_exp(disk.getDiskID(), disk.getCompany(), disk.getSpeed(), disk.getFreeSpace(), disk.getCost())}"
 
 def addDisk(disk: Disk) -> ReturnValue:
     """
@@ -136,18 +182,18 @@ def addDisk(disk: Disk) -> ReturnValue:
         free_space INT NOT NULL
         cost INT NOT NULL
     """
-    ret, res = sql_exe(f"INSERT INTO {dtable_name} VALUES({disk.getDiskID()}, {disk.getCompany()}, {disk.getSpeed()}, {disk.getFreeSpace()}, {disk.getCost()});")
+    ret, res = sql_exe(addDisk_query(disk))
     return ret
 
 def getDiskByID(diskID: int) -> Disk:
-    ret, (num_results, results) = sql_exe(f"SELECT * FROM {dtable_name} WHERE diskID = {diskID};")
+    ret, (num_results, results) = sql_exe(f"SELECT * FROM {dtable} WHERE diskID = {diskID};")
     if len(results) < 1:
         return Disk.badDisk()
     return Disk(*(results[0].values()))
 
 
 def deleteDisk(diskID: int) -> ReturnValue:
-    ret, res = sql_exe(f"DELETE FROM {dtable_name} WHERE diskID = {diskID};")
+    ret, res = sql_exe(f"DELETE FROM {dtable} WHERE diskID = {diskID};")
     return ret
 
 def addRAM(ram: RAM) -> ReturnValue:
@@ -156,62 +202,125 @@ def addRAM(ram: RAM) -> ReturnValue:
         company TEXT NOT NULL
         size INT NOT NULL
     """
-    ret, res = sql_exe(f"INSERT INTO {rtable_name} VALUES({ram.getRamID()}, {ram.getCompany()}, {ram.getSize()});")
+    ret, res = sql_exe(f"INSERT INTO {rtable} {create_values_exp(ram.getRamID(), ram.getCompany(), ram.getSize())}")
     return ret
 
 def getRAMByID(ramID: int) -> RAM:
-    ret, (num_results, results) = sql_exe(f"SELECT * FROM {rtable_name} WHERE ramID = {ramID};")
+    ret, (num_results, results) = sql_exe(f"SELECT * FROM {rtable} WHERE ramID = {ramID};")
     if len(results) < 1:
         return RAM.badRAM()
     return RAM(*(results[0].values()))
 
 def deleteRAM(ramID: int) -> ReturnValue:
-    ret, res = sql_exe(f"DELETE FROM {rtable_name} WHERE ramID = {ramID};")
+    ret, res = sql_exe(f"DELETE FROM {rtable} WHERE ramID = {ramID};")
     return ret
 
 def addDiskAndPhoto(disk: Disk, photo: Photo) -> ReturnValue:
-    return ReturnValue.OK
+    ret, res = sql_exe_transcation(addDisk_query(disk), addPhoto_query(photo))
+    return ret
 
-
+# Basic API:
 def addPhotoToDisk(photo: Photo, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
-
+    ret, res = sql_exe_transcation(#TODO: verify exceptions are handled correctly.
+        f"UPDATE {dtable} SET free_space=free_space-{photo.getSize()}",
+        f"INSERT INTO {podtable} {create_values_exp(photo.getPhotoID(), diskID)}"
+    )
+    return ret
 
 def removePhotoFromDisk(photo: Photo, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
+    ret, res = sql_exe_transcation(
+        f"UPDATE {dtable} SET free_space=free_space+{photo.getSize()}",
+        f"DELETE FROM {podtable} WHERE photoID={photo.getPhotoID()} AND diskID={diskID}"
+    )
+    return ret
 
 
 def addRAMToDisk(ramID: int, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
-
+    ret, res = sql_exe(f"INSERT INTO {rodtable} {create_values_exp(ramID, diskID)}")
+    return ret
 
 def removeRAMFromDisk(ramID: int, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
-
+    ret, res = sql_exe(f"DELETE FROM {rodtable} WHERE ramID={ramID} AND diskID={diskID}")
+    return ret
 
 def averagePhotosSizeOnDisk(diskID: int) -> float:
-    return 0
-
+    ret, res = sql_exe(f"SELECT AVG(size) FROM {ptable}, {podtable} WHERE {podtable}.photoID={ptable}.photoID AND {podtable}.diskID={diskID}")
+    if ret != ReturnValue.OK:
+        return -1
+    avg = res[1][0]['avg']
+    return 0 if avg == None else avg
 
 def getTotalRamOnDisk(diskID: int) -> int:
-    return 0
+    ret, res = sql_exe(f"SELECT SUM(size) FROM {rtable}, {rodtable} WHERE {rtable}.ramID={rodtable}.ramID AND {rodtable}.diskID={diskID}")
+    if ret != ReturnValue.OK:
+        return -1
+    sum = res[1][0]['sum']
+    return 0 if sum == None else sum
 
-
-def getCostForDescription(description: str) -> int:
-    return 0
-
+def getCostForDescription(description: str) -> int:#TODO: basic test
+    ret, res = sql_exe(f"SELECT SUM({dtable}.cost*{ptable}.size)\
+        FROM {ptable}, {podtable}, {dtable}\
+        WHERE {ptable}.description={description} AND {ptable}.photoID={podtable}.photoID AND {dtable}.diskID={podtable}.diskID"
+    )
+    if ret != ReturnValue.Ok:
+        return -1
+    sum = res[1][0]['sum']
+    return 0 if sum == None else sum
 
 def getPhotosCanBeAddedToDisk(diskID: int) -> List[int]:
-    return []
-
+    ret, res = sql_exe(f"SELECT {ptable}.photoID FROM {ptable}\
+        WHERE {ptable}.size <= (SELECT free_space FROM\
+            {dtable} WHERE {dtable}.diskID={diskID}\
+        )\
+        ORDER BY {ptable}.photoID DESC\
+        LIMIT 5"
+    )
+    if ret != ReturnValue.OK or res[1] == None:
+        return []
+    photo_ids = []
+    for i in range(res[0]):
+        d = res[1][i]
+        photo_ids.append(*d.values())
+    return photo_ids
+    
 
 def getPhotosCanBeAddedToDiskAndRAM(diskID: int) -> List[int]:
-    return []
-
+    ret, res = sql_exe(f"\
+        (\
+            SELECT {ptable}.photoID FROM {ptable}\
+            WHERE {ptable}.size <= (SELECT free_space FROM {dtable} WHERE {dtable}.diskID={diskID})\
+        )\
+            INTRERSECT\
+        (\
+            SELECT {ptable}.photoID FROM {ptable}\
+            WHERE {ptable}.size <=\
+            (\
+                SELECT SUM({rtable}.size) FROM {rtable}, {rodtable}\
+                WHERE {rtable}.ramID={rodtable}.ramID AND {rodtable}.diskID={diskID}\
+            )\
+        )\
+        ORDER BY {ptable}.photoID DESC\
+        LIMIT 5"
+    )
+    if ret != ReturnValue.OK or res[1] == None:
+        return []
+    photo_ids = []
+    for i in range(res[0]):
+        d = res[1][i]
+        photo_ids.append(*d.values())
+    return photo_ids
 
 def isCompanyExclusive(diskID: int) -> bool:
-    return True
-
+    ret, res = sql_exe(f"SELECT diskID FROM {dtable} WHERE\
+            (SELECT company FROM {dtable} WHERE {dtable}.diskID={diskID})\
+            =\
+            ALL(SELECT company FROM {rodtable}, {rtable} WHERE {rodtable}.ramID={rtable}.ramID) AND {rodtable}.diskID={diskID})\
+        AND\
+            {dtable}.diskID={diskID}\
+    ")
+    if ret != ReturnValue.OK or res[1] == None:
+        return False
+    return (res[0] != 0)
 
 def isDiskContainingAtLeastNumExists(description : str, num : int) -> bool:
     return True
@@ -220,14 +329,14 @@ def isDiskContainingAtLeastNumExists(description : str, num : int) -> bool:
 def getDisksContainingTheMostData() -> List[int]:
     return []
 
-
+# Advanced API:
 def getConflictingDisks() -> List[int]:
-    return []
+return []
 
 
 def mostAvailableDisks() -> List[int]:
-    return []
+return []
 
 
 def getClosePhotos(photoID: int) -> List[int]:
-    return []
+return []
--- Homework2/code/Tests/t1.py
+++ Homework2/code/Tests/t1.py
@@ -10,10 +10,85 @@ from Business.RAM import RAM
 from Business.Disk import Disk
 
 import Utility.DBConnector as Connector
+from Tests.abstractTest import AbstractTest
+
+# import inspect
+# def get_cur_func_name():
+#    print(inspect.stack()[0][3])
+
+class MyTest(AbstractTest):
+# Utilities ==================================================
+    def assertOk(self, value):
+        self.assertEqual(ReturnValue.OK, value)
+    
+    def init_items(self, *items: list):
+        for item in items:
+            if type(item) is RAM:
+                self.assertOk(Solution.addRAM(item))
+            elif type(item) is Disk:
+                self.assertOk(Solution.addDisk(item))
+            elif type(item) is Photo:
+                self.assertOk(Solution.addPhoto(item))
+        
+# Tests ==================================================
+    def t1_averagePhotosSizeOnDisk(self):
+        Solution.clearTables()
+        avl_tree = Photo(52, "tree", 20)
+        Cdrive = Disk(3, "dell-disks", 9001, 30, 33)
+        self.init_items(avl_tree, Cdrive)
+        self.assertOk(Solution.addPhotoToDisk(avl_tree, Cdrive.getDiskID()))
+        avg = Solution.averagePhotosSizeOnDisk(3)
+        self.assertEqual(avg, 20)
+        print("t1_averagePhotosSizeOnDisk finished succesfully")
+    
+    def t2_averagePhotosSizeOnDisk(self):
+        Solution.clearTables()
+        avl_tree = Photo(52, "tree", 20)
+        b23_tree = Photo(15, "bibi hamelech", 10)
+        Cdrive = Disk(3, "dell-disks", 9001, 30, 33)
+        self.init_items(Cdrive, avl_tree, b23_tree)
+        self.assertOk(Solution.addPhotoToDisk(avl_tree, Cdrive.getDiskID()))
+        self.assertOk(Solution.addPhotoToDisk(b23_tree, Cdrive.getDiskID()))
+        self.assertEqual(Solution.averagePhotosSizeOnDisk(Cdrive.getDiskID()), 15)
+
+        Ddrive = Disk(4, "sanDisk", 89, 5000, 30)
+        self.assertEqual(Solution.averagePhotosSizeOnDisk(Ddrive.getDiskID()), 0)
+        print("t2_averagePhotosSizeOnDisk finished succesfully")
+
+    def t3_getTotalRamOnDisk(self):
+        Solution.clearTables()
+        r1 = RAM(60, "hp", 300)
+        r2 = RAM(1, "hp", 700)
+        d = Disk(30, "hp", 9001, 90, 3)
+        d2 = Disk(1, "hp", 9001, 90, 3)
+        self.init_items(r1, r2, d, d2)
+
+        self.assertOk(Solution.addRAMToDisk(r1.getRamID(), d.getDiskID()))
+        self.assertOk(Solution.addRAMToDisk(r2.getRamID(), d.getDiskID()))
+        self.assertEqual(Solution.getTotalRamOnDisk(d.getDiskID()), 1000)
+
+        self.assertEqual(Solution.getTotalRamOnDisk(d2.getDiskID()), 0)
+        print("t3_getTotalRamOnDisk finished successfully")
+
+    def t4_getPhotosCanBeAddedToDisk(self):
+        Solution.clearTables()
+        p1 = Photo(111, "hp", 89)
+        p2 = Photo(222, "hp", 93)
+        p3 = Photo(333, "hp", 90)
+        p4 = Photo(444, "hp", 9001)
+        d = Disk(30, "hp", 9001, 90, 3)
+        self.init_items(p1, p2, p3, p4, d)
+
+        self.assertEqual(Solution.getPhotosCanBeAddedToDisk(d.getDiskID()), [
+            p3.getPhotoID(),
+            p1.getPhotoID(),
+        ])
+
+        print("t4_getPhotosCanBeAddedToDisk finished successfully")
 
 if __name__ == '__main__':
-    # Solution.createTables()
-    # Solution.addPhoto(Photo(52, "tree", 9001))
-    # Solution.addPhoto(Photo(42, "house", 4444))
-    # p = Solution.getPhotoByID(52)
-    # print(p)
+    t = MyTest()
+    t.t1_averagePhotosSizeOnDisk()
+    t.t2_averagePhotosSizeOnDisk()
+    t.t3_getTotalRamOnDisk()
+    t.t4_getPhotosCanBeAddedToDisk()
